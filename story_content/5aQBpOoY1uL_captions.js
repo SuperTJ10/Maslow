433691ceeabe7ad919b4375a6ec466b301351c6e
Tim Marshall <supertj10@gmail.com>
2020-07-29 15:26:52 +0100

    Add files via upload

---
+++ story_content/5aQBpOoY1uL_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.40.22208.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.579%0D%0AWelcome%20to%20this%20overview%20of%20Abraham%20Maslow%E2%80%99s%20Hierarchy%20of%20Needs.%0D%0A%0D%0A"}')
---
+++ story_content/69OnuM1ckGB_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.40.22208.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.970%0D%0AThat%20concludes%20this%20summary%20of%20Abraham%20Maslow%E2%80%99s%20Hierarchy%20of%20Needs.%0D%0A%0D%0A"}')
